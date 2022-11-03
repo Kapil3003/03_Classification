2f0580afa6f2e4ece4784ba54cdae56b90721019
Kapil3003 <41763456+Kapil3003@users.noreply.github.com>
2022-11-03 18:31:24 +0100

    Update classification_streamlit_app.py

--- classification_streamlit_app.py
+++ classification_streamlit_app.py
@@ -227,7 +227,7 @@ with tab1:
 	a = np.expand_dims(a, 0)
 	
 	"Hello"
-# 	predict_XGB = XGB_model.predict(a)[0]
+	predict_XGB = XGB_model.predict(a)[0]
 	predict_RF = RF_model.predict(a)[0]
 	predict_LR =  LR_model.predict(a)[0]
 	predict_XGB = 1
